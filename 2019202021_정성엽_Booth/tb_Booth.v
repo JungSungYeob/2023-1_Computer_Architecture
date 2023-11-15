59e776e1c5b74402d3fee3b3fe713ac5a99c900d
JungSungYeob <92677088+JungSungYeob@users.noreply.github.com>
2023-11-15 19:14:46 +0900

    Assignment 2
    Computer_Architecture Assignment 2

---
+++ 2019202021_정성엽_Booth/tb_Booth.v
@@ -0,0 +1,30 @@
+`timescale 1ns/1ps
+`include "Booth.v"
+module tb_booth;
+	reg [3:0] input1, input2;
+	reg clk, start, reset;
+	wire [7:0] result;
+	wire [1:0] count;
+	
+	booth Booth(.input1(input1),.input2(input2),.clk(clk),.start(start),.reset(reset),.result(result),.count(count));
+
+	always #5 clk= ~clk;
+
+	initial begin
+		$dumpfile("tb_booth.vcd");
+		$dumpvars(0,tb_booth);
+
+		reset =1;
+		start =1;
+		clk= 0;
+		reset =0;
+		#10;
+		start =0;
+		reset =1;
+		input1=4;
+		input2=5;
+		#10;
+		start =1;
+		#640; $finish;
+	end
+endmodule
